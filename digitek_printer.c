81ba7ea2d50791a5cbeef876d79718e20133dbd7
wincent.balin <wincent.balin@web.de>
2007-04-10 03:33:05 +0200

    Added some code for serial reading.
    darcs-hash:20070410013305-7847e-06f1d2b301f67be594ef9646df7e04782f3b70f2.gz

--- digitek_printer.c
+++ digitek_printer.c
@@ -9,12 +9,16 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <termios.h>
+#include <string.h>
 
 /* Program processing variables. */
 digitek_screen screen;
 digitek_state program_state = NONE;
 /* System variables. */
 int serial_port;
+struct termios serial_opts;
+unsigned char serial_input;
 
 /**
   Program entry.
@@ -28,23 +32,49 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
+	/* Prepare options for serial port. */
+/*
+	cfmakeraw(&serial_opts);
+	cfsetispeed(&serial_opts, B2400);
+*/
+	memset(&serial_opts, 0, sizeof(struct termios));
+	serial_opts.c_cflag = B2400 | CS8 | CLOCAL | CREAD;
+	serial_opts.c_iflag = IGNPAR;
+	serial_opts.c_oflag = 0;
+	serial_opts.c_lflag = 0;
+	serial_opts.c_cc[VMIN] = 1;	/* Receive at least one character. */
+	serial_opts.c_cc[VTIME] = 20;	/* Time-out after 2 seconds. */
+
 	/* Open serial terminal. */
-	serial_port = open(argv[1], O_RDONLY | O_NONBLOCK);
+	serial_port = open(argv[1], O_RDONLY | O_NONBLOCK | O_NOCTTY);
 	if(serial_port == -1)		/* Handle error. */
 	{
 		fprintf(stderr, "Could not open serial tty %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
+	/* Set terminal to prepared options. */
+	tcflush(serial_port, TCIFLUSH);
+	tcsetattr(serial_port, TCSANOW, &serial_opts);
+
 	/* Processing loop. */
 	while(1)
 	{
+		/* Read one character. */
+		read(serial_port, &serial_input, sizeof(unsigned char));
+
+		/* Process the read character. */
 		switch(program_state)
 		{
 			case NONE:
 				break;
 
 			case BYTE1:
+				if((serial_input >> 4) == 1)
+				{
+					/* Proceed to the next byte. */
+					program_state = BYTE2;
+				}
 				break;
 
 			case BYTE2:
