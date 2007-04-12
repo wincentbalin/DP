1cbac93dd60163f76c404ac3e9111ec75bb0678b
wincent.balin <wincent.balin@web.de>
2007-04-12 02:07:45 +0200

    Began to implement Ocaml version.
    darcs-hash:20070412000745-7847e-4bb70010d604de4a8b3277efa6d8cbfea48fee4e.gz

---
+++ digitek_printer.ml
@@ -0,0 +1,84 @@
+(*
+	Digitek DT-9062 screen printer.
+*)
+
+(* Import UNIX system functions. *)
+open Unix;;
+
+(* Define data types. *)
+type digitek_state =	BYTE1 |
+			BYTE2 |
+			BYTE3 |
+			BYTE4 |
+			BYTE5 |
+			BYTE6 |
+			BYTE7 |
+			BYTE8 |
+			BYTE9 |
+			BYTE10 |
+			BYTE11 |
+			BYTE12 |
+			BYTE13 |
+			BYTE14 ;;
+type digitek_screen = {	rs232	: bool;
+			auto	: bool;
+			dc	: bool;
+			ac	: bool;
+			a1	: bool;
+			a6	: bool;
+			a5	: bool;
+			sign	: bool;
+			a2	: bool;
+			a7	: bool;
+			a3	: bool;
+			a4	: bool;
+			b1	: bool;
+			b6	: bool;
+			b5	: bool;
+			p1	: bool;
+			b2	: bool;
+			b7	: bool;
+			b3	: bool;
+			b4	: bool;
+			c1	: bool;
+			c6	: bool;
+			c5	: bool;
+			p2	: bool;
+			c2	: bool;
+			c7	: bool;
+			c3	: bool;
+			c4	: bool;
+			d1	: bool;
+			d6	: bool;
+			d5	: bool;
+			p3	: bool;
+			d2	: bool;
+			d7	: bool;
+			d3	: bool;
+			d4	: bool;
+			diode	: bool;
+			kilo	: bool;
+			nano	: bool;
+			micro	: bool;
+			ton	: bool;
+			mega	: bool;
+			percent	: bool;
+			milli	: bool;
+			hold	: bool;
+			delta	: bool;
+			ohm	: bool;
+			farad	: bool;
+			batt	: bool;
+			hz	: bool;
+			volt	: bool;
+			ampere	: bool;
+			hfe	: bool;
+			celsius	: bool };;
+
+
+(* Auxilary functions. *)
+
+
+(* Main function. *)
+
+
